b3009281c8130e3e7bf7287f4bb2bc9700745a3c
cguanzon <cguanzon@instructure.com>
2014-09-01 12:08:28 -0600

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name feedScoreApp
+ * @description
+ * # feedScoreApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('feedScoreApp', ['ui.router']);
