f1bafdb782343044100c6440302a0b18012e4299
cguanzon <cguanzon@instructure.com>
2014-09-17 07:22:43 -0600

    begins work on compare

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -83,44 +83,45 @@ angular.module('feedScoreApp')
                             animation: {
                                 duration: 2000
                             }
-                        }
-                    },
-                    title: {
-                        text: 'Like/Comments Per Times Used for Recently Used Filters'
-                    },
-                    tooltip: {
-                        pointFormat: '{series.name}: {point.y:.2f}'
-                    },
-                    xAxis: {
-                        title: {
-                            text: 'Filter',
-                            style: chartTextStyle
                         },
-                        type: 'category',
-                        labels: {
-                            rotation: -45,
-                            style: chartTextStyle
+                        title: {
+                            text: 'Like/Comments Per Times Used for Recently Used Filters'
+                        },
+                        tooltip: {
+                            pointFormat: '{series.name}: {point.y:.2f}'
+                        },
+                        xAxis: {
+                            title: {
+                                text: 'Filter',
+                                style: chartTextStyle
+                            },
+                            type: 'category',
+                            labels: {
+                                rotation: -45,
+                                style: chartTextStyle
+                            }
+                        },
+
+                        yAxis: {
+                            min: 0,
+                            title: {
+                                text: 'Likes/Comments per Filter',
+                                style: chartTextStyle
+                            }
+                        },
+                        legend: {
+                            align: 'right',
+                            x: -70,
+                            verticalAlign: 'top',
+                            y: 20,
+                            floating: true,
+                            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+                            borderColor: '#CCC',
+                            borderWidth: 1,
+                            shadow: false
                         }
                     },
 
-                    yAxis: {
-                        min: 0,
-                        title: {
-                            text: 'Likes/Comments per Filter',
-                            style: chartTextStyle
-                        }
-                    },
-                    legend: {
-                        align: 'right',
-                        x: -70,
-                        verticalAlign: 'top',
-                        y: 20,
-                        floating: true,
-                        backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
-                        borderColor: '#CCC',
-                        borderWidth: 1,
-                        shadow: false
-                    },
                     series: [{
                         name: 'Likes per Times Used',
                         data: $scope.likesPerFilterChartData
@@ -141,30 +142,32 @@ angular.module('feedScoreApp')
                             animation: {
                                 duration: 900
                             }
+                        },
+                        title: {
+                            text: 'Amount of Times Used per Filter'
+                        },
+                        tooltip: {
+                            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                        },
+                        plotOptions: {
+                            pie: {
+                                dataLabels: {
+                                    enabled: true,
+                                    distance: -50,
+                                    style: {
+                                        fontWeight: 'bold',
+                                        color: 'white',
+                                        textShadow: '0px 1px 2px black'
+                                    }
+                                },
+                                startAngle: -90,
+                                endAngle: 90,
+                                center: ['50%', '75%']
+                            }
                         }
+
                     },
-                    title: {
-                        text: 'Amount of Times Used per Filter'
-                    },
-                    tooltip: {
-                        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-                    },
-                    plotOptions: {
-                        pie: {
-                            dataLabels: {
-                                enabled: true,
-                                distance: -50,
-                                style: {
-                                    fontWeight: 'bold',
-                                    color: 'white',
-                                    textShadow: '0px 1px 2px black'
-                                }
-                            },
-                            startAngle: -90,
-                            endAngle: 90,
-                            center: ['50%', '75%']
-                        }
-                    },
+
                     series: [{
                         type: 'pie',
                         name: 'Times Used',
